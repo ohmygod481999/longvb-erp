fd362d853d6a93e56e94cc69e9417b0f96df0b50
longvb <vuongbaolong48@gmail.com>
2022-08-10 08:42:37 +0000

    rm unused files

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -236,9 +236,9 @@ const Login = (props) => {
                                     </CardBody>
                                 </Card>
 
-                                <div className="mt-4 text-center">
+                                {/* <div className="mt-4 text-center">
                                     <p className="mb-0">Don't have an account ? <Link to="/register" className="fw-semibold text-primary text-decoration-underline"> Signup </Link> </p>
-                                </div>
+                                </div> */}
 
                             </Col>
                         </Row>
