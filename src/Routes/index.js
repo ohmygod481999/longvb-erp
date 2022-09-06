47d3ede629cf51e00a411633660b2ef8b28bf6f9
longvb <vuongbaolong48@gmail.com>
2022-09-06 10:01:12 +0700

    hashrouter

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,40 +1,25 @@
-import React from 'react';
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Switch, Route, HashRouter } from "react-router-dom";
 
 //Layouts
 import NonAuthLayout from "../Layouts/NonAuthLayout";
 import VerticalLayout from "../Layouts/index";
 //routes
 import { authProtectedRoutes, publicRoutes } from "./allRoutes";
-import { AuthProtected, AccessRoute } from './AuthProtected';
+import { AuthProtected, AccessRoute } from "./AuthProtected";
 
 const Index = () => {
     const availablePublicRoutesPaths = publicRoutes.map((r) => r.path);
     const availableAuthRoutesPath = authProtectedRoutes.map((r) => r.path);
     return (
         <React.Fragment>
-            <Switch>
-                <Route path={availablePublicRoutesPaths}>
-                    <NonAuthLayout>
-                        <Switch>
-                            {publicRoutes.map((route, idx) => (
-                                <Route
-                                    path={route.path}
-                                    component={route.component}
-                                    key={idx}
-                                    exact={true}
-                                />
-                            ))}
-                        </Switch>
-                    </NonAuthLayout>
-                </Route>
-
-                <Route path={availableAuthRoutesPath}>
-                    <AuthProtected>
-                        <VerticalLayout>
+            <HashRouter hashType="slash">
+                <Switch>
+                    <Route path={availablePublicRoutesPaths}>
+                        <NonAuthLayout>
                             <Switch>
-                                {authProtectedRoutes.map((route, idx) => (
-                                    <AccessRoute
+                                {publicRoutes.map((route, idx) => (
+                                    <Route
                                         path={route.path}
                                         component={route.component}
                                         key={idx}
@@ -42,10 +27,27 @@ const Index = () => {
                                     />
                                 ))}
                             </Switch>
-                        </VerticalLayout>
-                    </AuthProtected>
-                </Route>
-            </Switch>
+                        </NonAuthLayout>
+                    </Route>
+
+                    <Route path={availableAuthRoutesPath}>
+                        <AuthProtected>
+                            <VerticalLayout>
+                                <Switch>
+                                    {authProtectedRoutes.map((route, idx) => (
+                                        <AccessRoute
+                                            path={route.path}
+                                            component={route.component}
+                                            key={idx}
+                                            exact={true}
+                                        />
+                                    ))}
+                                </Switch>
+                            </VerticalLayout>
+                        </AuthProtected>
+                    </Route>
+                </Switch>
+            </HashRouter>
         </React.Fragment>
     );
 };
