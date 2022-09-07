e6724f27ec2576c221329ff609df8517583c359e
longvb <vuongbaolong48@gmail.com>
2022-09-07 14:29:35 +0700

    update dropout btn

--- src/Components/Common/ProfileDropdown.js
+++ src/Components/Common/ProfileDropdown.js
@@ -39,7 +39,7 @@ const ProfileDropdown = () => {
                 <DropdownMenu className="dropdown-menu-end">
 
                     <h6 className="dropdown-header">Welcome {userProfile.name}!</h6>
-                    <DropdownItem href="/profile"><i className="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
+                    <DropdownItem href="/#/profile"><i className="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                         <span className="align-middle">Profile</span></DropdownItem>
                     <DropdownItem href="/apps-chat"><i
                         className="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span
@@ -60,7 +60,7 @@ const ProfileDropdown = () => {
                                 className="align-middle">Settings</span></DropdownItem>
                     <DropdownItem href="/auth-lockscreen-basic"><i
                         className="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span className="align-middle">Lock screen</span></DropdownItem>
-                    <DropdownItem href="/logout"><i
+                    <DropdownItem href="/#/logout"><i
                         className="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span
                             className="align-middle" data-key="t-logout">Logout</span></DropdownItem>
                 </DropdownMenu>
